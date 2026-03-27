7382de2a4e6d008510dc5f9a27dff4f2f77c3f9a
Xantech007 <113646174+Xantech007@users.noreply.github.com>
2026-03-27 08:06:10 +0100

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -2,7 +2,7 @@
 ob_start();
 session_start();
 
-$conn = new mysqli('localhost', 'root' , '' , 'tickit');
+$conn = new mysqli('sql106.infinityfree.com', 'if0_41489345_event' , '1V1DyYiXWU4yVzp' , 'if0_41489345');
 if($conn->connect_error){
 	die("connection failed :" .$conn->connect_error);
 }
